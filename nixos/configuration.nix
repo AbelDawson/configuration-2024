937ac2c2f2e7a28690e0733fe132625f2efe3540
Abel Dawson <abeldattsun@gmail.com>
2024-06-13 20:55:17 -0700

    Updated for Nixos 24.05

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./disable.nix
     ];
 
   # Bootloader.
@@ -44,8 +45,8 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # sound
@@ -63,7 +64,6 @@
 
   programs.hyprland = {
      enable = true;
-     enableNvidiaPatches = true;
      xwayland.enable = true;
   };
 
@@ -74,7 +74,7 @@
   hardware = {
       opengl.enable = true;
 
-      nvidia.modesetting.enable = true;
+      # nvidia.modesetting.enable = true;
   };
 
   security.pam.services.swaylock = {};
@@ -95,6 +95,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      neovim
+     
      pkgs.waybar
      (pkgs.waybar.overrideAttrs (oldAttrs: {
      	mesonFlags = oldAttrs.mesonFlags ++["-Dexperimental=true"];
@@ -136,8 +137,29 @@
 
      pkgs.libgcc
      pkgs.fzf
+     pkgs.vimPlugins.fzfWrapper
 
      pkgs.networkmanagerapplet
+
+     pkgs.htop
+
+     pkgs.brightnessctl
+
+     pkgs.gimp
+  ];
+
+
+  fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      font-awesome
   ];
 
   #desktop portals
@@ -155,7 +177,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+
+  # battery management
+  services.tlp.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -170,5 +195,10 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+
+  system.copySystemConfiguration = true;
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.05";
 
 }
